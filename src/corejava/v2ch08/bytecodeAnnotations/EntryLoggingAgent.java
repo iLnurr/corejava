394f10cf8d61836476a0697bbae6e526334e0679
ilnur <serbaevilnur@gmail.com>
2016-09-19 17:12:29 +0500

    addition files of the book

---
+++ src/corejava/v2ch08/bytecodeAnnotations/EntryLoggingAgent.java
@@ -0,0 +1,43 @@
+package corejava.v2ch08.bytecodeAnnotations;
+
+import org.apache.bcel.classfile.*;
+import org.apache.bcel.generic.*;
+
+import java.io.ByteArrayInputStream;
+import java.lang.instrument.ClassFileTransformer;
+import java.lang.instrument.Instrumentation;
+import java.security.ProtectionDomain;
+
+/**
+ * @version 1.00 2004-08-17
+ * @author Cay Horstmann
+ */
+public class EntryLoggingAgent
+{
+   public static void premain(final String arg, Instrumentation instr)
+   {
+      instr.addTransformer(new ClassFileTransformer()
+         {
+            public byte[] transform(ClassLoader loader, String className, Class<?> cl,
+                  ProtectionDomain pd, byte[] data)
+            {
+               if (!className.equals(arg)) return null;
+               try
+               {
+                  ClassParser parser = new ClassParser(new ByteArrayInputStream(data), className
+                        + ".java");
+                  JavaClass jc = parser.parse();
+                  ClassGen cg = new ClassGen(jc);
+                  EntryLogger el = new EntryLogger(cg);
+                  el.convert();
+                  return cg.getJavaClass().getBytes();
+               }
+               catch (Exception e)
+               {
+                  e.printStackTrace();
+                  return null;
+               }
+            }
+         });
+   }
+}
